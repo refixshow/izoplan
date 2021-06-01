a0a13ae27f5bf57739ece9e62d903637eb20b699
Adam Path <adamscieszka@gmail.com>
2021-06-01 09:20:42 +0200

    first commit

--- components/organisms/OrganismFooter.tsx
+++ components/organisms/OrganismFooter.tsx
@@ -1,5 +1,7 @@
-import { Flex, Box } from "@chakra-ui/react";
+import { Flex, Box, Text } from "@chakra-ui/react";
 import { MoleculeFooterNav } from "../molecules";
+import NextLink from "next/link";
+import { Link as ChakraLink } from "@chakra-ui/react";
 
 const OrganismFooter = () => {
   return (
@@ -17,7 +19,19 @@ const OrganismFooter = () => {
       <Box margin="2">logo fb</Box>
       <Box margin="2">pomoc</Box>
       <Box margin="2">izoplan</Box>
-      <Box margin="2">&copy; wszelkie prawa zastrzeżone przez izoplan.</Box>
+      <Box margin="2">
+        <Text>
+          &copy;{" "}
+          <NextLink
+            passHref
+            href="https://www.freeprivacypolicy.com/live/85312fcc-e02f-4357-9de2-da390f55fd83"
+          >
+            <ChakraLink target="_blank">
+              wszelkie prawa zastrzeżone przez izoplan.
+            </ChakraLink>
+          </NextLink>
+        </Text>
+      </Box>
     </Flex>
   );
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -8,6 +8,13 @@ import {
   Portal,
   useDisclosure,
   Button,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
 } from "@chakra-ui/react";
 
 import { useMemo } from "react";
@@ -36,8 +43,7 @@ interface IProps {
 
 const Home = ({ contentfulData }: IProps) => {
   const DarkColor = useColorModeValue("#eee", theme.colors.gray[700]);
-
-  const { isOpen, onToggle } = useDisclosure();
+  const modal = useDisclosure();
 
   const parsedcontentfulData = useMemo(
     () => parseContentfulData(contentfulData),
@@ -55,26 +61,51 @@ const Home = ({ contentfulData }: IProps) => {
       }}
     >
       <Portal appendToParentPortal={false}>
-        <Flex
-          zIndex="1000"
+        <Button
           position="fixed"
-          right={isOpen ? "0" : "-340px"}
-          top="15%"
+          top="300"
+          right="5"
+          css={{ transform: "rotate(-90deg)", transformOrigin: "right" }}
+          colorScheme="facebook"
+          onClick={modal.onOpen}
         >
-          <Button colorScheme="facebook" onClick={onToggle}>
-            Facebook
-          </Button>
-          <iframe
-            src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FIzoplan-izolacje-pian%25C4%2585-PUR-102063341422126&tabs=timeline&width=340&height=500&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId=1158616097919420"
-            width="340"
-            height="500"
-            style={{ border: "none", overflow: "hidden" }}
-            scrolling="no"
-            frameBorder="0"
-            allowFullScreen
-            allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"
-          ></iframe>
-        </Flex>
+          Facebook
+        </Button>
+
+        <Modal isCentered isOpen={modal.isOpen} onClose={modal.onClose}>
+          <ModalOverlay />
+          <ModalContent>
+            <ModalHeader>Ciepło polecamy!</ModalHeader>
+            <ModalCloseButton />
+            <ModalBody>
+              <Flex
+                justifyContent="center"
+                alignItems="center"
+                textAlign="center"
+                height="50vh"
+              >
+                <iframe
+                  src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FIzoplan-izolacje-pian%25C4%2585-PUR-102063341422126&tabs=timeline&width=340&height=500&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId=1158616097919420"
+                  height="340"
+                  style={{
+                    border: "none",
+                    overflow: "hidden",
+                  }}
+                  scrolling="no"
+                  frameBorder="0"
+                  allowFullScreen
+                  allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"
+                ></iframe>
+              </Flex>
+            </ModalBody>
+
+            <ModalFooter>
+              <Button colorScheme="blue" mr={3} onClick={modal.onClose}>
+                Zamknij
+              </Button>
+            </ModalFooter>
+          </ModalContent>
+        </Modal>
       </Portal>
       <MoleculeMainNavBar />
       <MoleculeHero />

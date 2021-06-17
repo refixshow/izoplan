cc15725f3f3da374ef0275325bfae51a303f1071
Adam Path <adamscieszka@gmail.com>
2021-06-17 15:05:58 +0200

    first commit

--- lib/contentful.ts
+++ lib/contentful.ts
@@ -56,11 +56,12 @@ export const fetchAndParseFacebookReviews = async () => {
       {
         params: {
           fields: "access_token",
-          access_token:
-            "EAAEpDMYI6WsBALDHNjIbFaeao5vXtVOtS1ya0AcUdgSoW97kZBrc3Qmj48L2WB808TZCwuBLZBoc9sSW0jcDbUigtoQb21jx1NfNGDsFDMy4daICB2njGKyzWfgBYzRXuVb7kqwHZANZAU3qZCwxl3YQGnnN4zsurjieYnKBZBdZAjmYZBaGnccNNXA69wDlwfQpsT0y4ameK7hdZB4If56TEkdsakLQx3MBcZD",
+          access_token: process.env.NEXT_PUBLIC_FB_TOKEN,
         },
       }
     );
+
+    console.log(resWithToken);
 
     res = await axios.get("https://graph.facebook.com/v10.0/102063341422126", {
       params: {
@@ -70,7 +71,7 @@ export const fetchAndParseFacebookReviews = async () => {
       },
     });
   } catch (err) {
-    console.error(err);
+    console.error("avx");
   }
 
   return res.data.ratings.data;

136be1809ad1828a35156687ebdaf77d8a765cf2
Adam Path <adamscieszka@gmail.com>
2021-06-29 16:25:24 +0200

    first commit

--- components/organisms/offer/OrganismSectionOffer.tsx
+++ components/organisms/offer/OrganismSectionOffer.tsx
@@ -1,12 +1,11 @@
 import { FC } from "react";
 
 interface IProps {
-  offer: { [key: string]: any }[];
+  offer: { [key: string]: any };
 }
 
 const OrganismSectionOffer: FC<IProps> = ({ offer }) => {
-  console.log(offer);
-  return <div>{offer[0].fields.title}</div>;
+  return <pre>{JSON.stringify(offer, null, 2)}</pre>;
 };
 
 export default OrganismSectionOffer;
